9b4049de1301ef341ce07c5d4e31ee0903667164
M.Alp <mehmetalp.taser@bahcesehir.edu.tr>
2022-03-01 02:19:07 +0300

    Video Modeli JSON ve akışa göre düzenlendi.

--- YoutubeClone/Video.swift
+++ YoutubeClone/Video.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+struct Video {
+    let title : String
+    let channel : String
+    let view: String
+    let date: String
+    let video_key:String
+    let tags: [String]
+    
+    var image_url: URL {
+          return URL(string: "https://img.youtube.com/vi/\(video_key)/0.jpg")!
+      }
+    
+    var video_url: URL {
+          return URL(string: "https://www.youtube.com/embed/\(video_key)")!
+      }
+      
+    var tag: String {
+            return tags.shuffled().joined(separator: " ")
+        }
+    
+    var shortDescription: String {
+            return "\(channel) - \(view) - \(date)"
+        }
+        
+        var detailDescription: String {
+            return "\(view) - \(date)"
+        }
+        
+    }
