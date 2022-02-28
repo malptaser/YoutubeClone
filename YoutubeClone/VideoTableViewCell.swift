58ab72bab048d2d5649260e5a4f2ce616ee7e46a
M.Alp <mehmetalp.taser@bahcesehir.edu.tr>
2022-03-01 02:29:37 +0300

    TableView Cell Düzenlendi.

--- YoutubeClone/VideoTableViewCell.swift
+++ YoutubeClone/VideoTableViewCell.swift
@@ -12,8 +12,7 @@ class VideoTableViewCell: UITableViewCell {
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var tags: UILabel!
     @IBOutlet weak var title: UILabel!
-    @IBOutlet weak var shortDecscription: UILabel!
-    
+    @IBOutlet weak var shortDescription: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -25,4 +24,27 @@ class VideoTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    func prepare(video: Video) {
+       
+        tags.text = video.tag
+        title.text = video.title
+        shortDescription.text = video.shortDescription
+        img.load(url: video.image_url)
+        
+    }
+    
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
